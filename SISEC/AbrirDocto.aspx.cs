684da420efe0a6128e1e439e0ef5264b86b5a4a6
rigo1190 <rigo1190@gmail.com>
2014-10-14 20:00:20 -0500

    Se carga cs de AbrirDocto

---
+++ SISEC/AbrirDocto.aspx.cs
@@ -0,0 +1,24 @@
+﻿using BL;
+using DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SISEC
+{
+    public partial class AbrirDocto : System.Web.UI.Page
+    {
+        private UnitOfWork uow;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            uow = new UnitOfWork();
+            int id = Utilerias.StrToInt(Request.Params["i"].ToString());
+            Normatividad obj = uow.NormatividadBusinessLogic.GetByID(id);
+            Response.Redirect("~/ArchivosNormatividad/" + obj.ID + "/" + obj.NombreArchivo);
+
+        }
+    }
+}
